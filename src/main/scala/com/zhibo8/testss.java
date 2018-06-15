7dbdc512f0fedd10a8b797de20db63fcc7429cf7
cjp <378344307@qq.com>
2018-06-15 09:50:39 +0800

    无

--- src/main/scala/com/zhibo8/testss.java
+++ src/main/scala/com/zhibo8/testss.java
@@ -1,15 +1,8 @@
 package com.zhibo8;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class testss {
     public static void main(String[] args) {
-
-        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
-        for (Map.Entry<String, Object> stringObjectEntry : stringObjectHashMap.entrySet()) {
-            String key = stringObjectEntry.getKey();
-            String value = String.valueOf(stringObjectEntry.getValue());
-        }
+        System.out.println("world ");
     }
 }
